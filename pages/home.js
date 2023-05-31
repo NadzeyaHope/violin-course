e28564f82c9e3e16ad6f70baf8a81750c1642f52
Nadzeya <radzkovanadzeya@gmail.com>
2023-05-31 18:10:47 +0300

    Fix 1

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,9 @@
 import Navigation from '../ui/components/Navigation.js';
 import Layout from '../ui/components/Layout.js';
 import TextAboutUs from '../ui/sections/home/TextAboutUs.js';
-import PriceSection from '../ui/sections/home/priceSection.js';
-import Classes from '../ui/sections/home/classes.js';
-import Footer from '../ui/sections/home/footer.js'
+import PriceSection from '../ui/sections/home/PriceSection.js';
+import Classes from '../ui/sections/home/Classes.js';
+import Footer from '../ui/sections/home/Footer.js';
 import HeroSection from '../ui/components/HeroSection.js';
 
 export default (params) => {
